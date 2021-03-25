4172b1277901936a6ca3338c3353f61a72a4734b
ryanspivey <rlspivey@go.olemiss.edu>
2021-03-25 15:16:14 -0500

    task 1 & 2 complete

--- index.js
+++ index.js
@@ -42,7 +42,21 @@ class Airplane {
   */
   
  class Person {
-    
+    constructor(name, age){
+      this.name = name;
+      this.age = age;
+      this.stomach = [];
+    }
+    eat(food) {
+      if(this.stomach.length < 10)
+        this.stomach.push(food);
+    }
+    poop() {
+      this.stomach = [];
+    }
+    toString() {
+      return `${this.name}, ${this.age}`;
+    }
   }
   
   /*
@@ -60,7 +74,31 @@ class Airplane {
   */
   
  class Car {
-    
+    constructor(model, milesPerGallon){
+      this.model = model;
+      this.milesPerGallon = milesPerGallon;
+      this.tank = 0;
+      this.odometer = 0;
+    }
+    fill(gallons){
+      this.tank += gallons;
+    }
+    drive(distance){
+      if(this.tank > 0){
+        if((this.tank - distance/this.milesPerGallon) > 0){
+          this.odometer += distance;
+          this.tank -= distance/this.milesPerGallon;
+        }
+        else {
+          this.odometer += (this.tank * this.milesPerGallon);
+          this.tank = 0;
+          return `I ran out of fuel at ${this.odometer} miles!`;
+        }
+      }
+      else{
+        return `I ran out of fuel at ${this.odometer} miles!`;
+      }
+    }
   }
   
   /*
